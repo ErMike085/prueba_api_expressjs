1a26d3559de5b3a094f31adad504397e80575ed1
ErMike085 <mmunozortiz706@gmail.com>
2023-06-23 23:44:08 -0500

    arreglando rutas

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 const app = express();
 import cors from "cors";
-import { pool } from "./src/db.js";
-import { PORT } from "./src/config.js";
+import { pool } from "src/db.js";
+import { PORT } from "src/config.js";
 
 app.use(cors());
 app.use(express.json());
